e127d1659a89221959587ea44162b735f45b42bf
eagullof <eagullof@fpvirtualaragon.es>
2025-03-04 10:04:31 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Habitos Saludables',
+  webDir: 'www'
+};
+
+export default config;
